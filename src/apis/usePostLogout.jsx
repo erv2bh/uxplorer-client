ff72202292dcffa0df6466db07c539875e73ad61
erv2bh <pagaous@gmail.com>
2024-03-12 21:07:43 +0900

    feat: add logout hook

---
+++ src/apis/usePostLogout.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSetAtom } from "jotai";
+
+import fetchData from "../utils/axios";
+
+import userAtom from "../atoms/atoms";
+
+function useLogout() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const setUser = useSetAtom(userAtom);
+
+  async function handleGoogleLogout() {
+    await fetchData("POST", "/auth/logout");
+  }
+
+  const { mutate: fetchLogout } = useMutation({
+    mutationFn: handleGoogleLogout,
+    onSuccess: () => {
+      setUser("");
+      queryClient.clear();
+      navigate("/");
+    },
+  });
+
+  return fetchLogout;
+}
+
+export default useLogout;
