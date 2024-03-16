b9da62c9201022b6d97bafedfaa4a25375a20894
erv2bh <pagaous@gmail.com>
2024-03-17 02:27:29 +0900

    feat: add routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,9 +4,13 @@ import Dashboard from "../components/Dashboard";
 
 import GlobalStyle from "../components/Styles";
 import NewTest from "../components/NewTest";
-import TestDetail from "../components/NewTest/TestDetail";
-import TestMission from "../components/NewTest/TestMission";
+import TestDetailForm from "../components/NewTest/TestDetailForm";
+import TestMissionForm from "../components/NewTest/TestMissionForm";
 import SubmissionCheck from "../components/NewTest/SubmissionCheck";
+import TestDetail from "../components/TestDetail";
+import TestInfo from "../components/TestDetail/TestInfo";
+import TestResult from "../components/TestDetail/TestResult";
+import UserResults from "../components/TestDetail/TestResult/UserResults";
 
 function App() {
   return (
@@ -16,9 +20,15 @@ function App() {
         <Route path="/" element={<Onboarding />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/new-test" element={<NewTest />}>
-          <Route path="test-detail" element={<TestDetail />} />
-          <Route path="test-mission" element={<TestMission />} />
+          <Route path="test-detail" element={<TestDetailForm />} />
+          <Route path="test-mission" element={<TestMissionForm />} />
           <Route path="submission-check" element={<SubmissionCheck />} />
+        </Route>
+        <Route path="/test/:testId" element={<TestDetail />}>
+          <Route path="test-info" element={<TestInfo />} />
+          <Route path="test-result" element={<TestResult />}>
+            <Route path="user-results" element={<UserResults />} />
+          </Route>
         </Route>
       </Routes>
     </>
