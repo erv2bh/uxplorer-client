05c3186c4a7aec0d7dd358a49cef598771e24526
erv2bh <pagaous@gmail.com>
2024-03-17 20:08:11 +0900

    feat: add access/deny logic in router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { useAtomValue } from "jotai";
+import { userAtom } from "../atoms/atoms";
+
 import Onboarding from "../components/Onboarding";
 import Dashboard from "../components/Dashboard";
 
@@ -11,25 +15,34 @@ import TestDetail from "../components/TestDetail";
 import TestInfo from "../components/TestDetail/TestInfo";
 import TestResult from "../components/TestDetail/TestResult";
 import UserResults from "../components/TestDetail/TestResult/UserResults";
+import TotalResults from "../components/TestDetail/TestResult/TotalResult";
 
 function App() {
+  const user = useAtomValue(userAtom);
+
   return (
     <>
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Onboarding />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/new-test" element={<NewTest />}>
-          <Route path="test-detail" element={<TestDetailForm />} />
-          <Route path="test-mission" element={<TestMissionForm />} />
-          <Route path="submission-check" element={<SubmissionCheck />} />
-        </Route>
-        <Route path="/test/:testId" element={<TestDetail />}>
-          <Route path="test-info" element={<TestInfo />} />
-          <Route path="test-result" element={<TestResult />}>
-            <Route path="user-results" element={<UserResults />} />
-          </Route>
-        </Route>
+        {user && (
+          <>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/new-test" element={<NewTest />}>
+              <Route path="test-detail" element={<TestDetailForm />} />
+              <Route path="test-mission" element={<TestMissionForm />} />
+              <Route path="submission-check" element={<SubmissionCheck />} />
+            </Route>
+            <Route path="/test/:testId" element={<TestDetail />}>
+              <Route path="test-info" element={<TestInfo />} />
+              <Route path="test-result" element={<TestResult />}>
+                <Route path="user-results" element={<UserResults />} />
+                <Route path="total-results" element={<TotalResults />} />
+              </Route>
+            </Route>
+          </>
+        )}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
