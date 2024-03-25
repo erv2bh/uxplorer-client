0762fad1cbf8f18695783716daf3cb0a26e12f23
erv2bh <pagaous@gmail.com>
2024-03-25 12:53:30 +0900

    feat: set global states for total results

--- src/apis/useGetSingleTest.jsx
+++ src/apis/useGetSingleTest.jsx
@@ -10,6 +10,9 @@ import {
   currentTestTitleAtom,
   missionsDataAtom,
   testerDataAtom,
+  completedMissionDataAtom,
+  completedMissionCountAtom,
+  completedTesterAtom,
 } from "../atoms/atoms";
 
 import Loading from "../components/shared/Loading";
@@ -22,10 +25,24 @@ function useGetSingleTest() {
   const setMissionsData = useSetAtom(missionsDataAtom);
   const setCurrentTestTitle = useSetAtom(currentTestTitleAtom);
   const setTesterDataAtom = useSetAtom(testerDataAtom);
+  const setCompletedMissionCount = useSetAtom(completedMissionCountAtom);
+  const setCompletedMissionData = useSetAtom(completedMissionDataAtom);
+  const setCompletedTester = useSetAtom(completedTesterAtom);
 
   async function getSingleTest() {
     const response = await fetchData("GET", `users/${userId}/tests/${testId}`);
 
+    const totalMissions = response.data.missions.length;
+    const completedTesters = response.data.loggedInTesters.length;
+    const totalCompletedMissionsCount = totalMissions * completedTesters;
+    const completedMissionsCount = response.data.completedMission.length;
+
+    setCompletedTester(response.data.loggedInTesters);
+    setCompletedMissionCount({
+      completedMissionsCount,
+      totalCompletedMissionsCount,
+    });
+    setCompletedMissionData(response.data.completedMission);
     setCurrentTestData(response.data.test);
     setCurrentTestTitle(response.data.test.title);
     setMissionsData(response.data.missions);
