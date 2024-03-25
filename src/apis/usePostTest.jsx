846420501295a36a3224e6b68a05e43b29717f5f
erv2bh <pagaous@gmail.com>
2024-03-25 12:58:42 +0900

    feat: add error messages when failed to create test

--- src/apis/usePostTest.jsx
+++ src/apis/usePostTest.jsx
@@ -1,17 +1,23 @@
 import { useNavigate } from "react-router-dom";
 
 import { useMutation } from "@tanstack/react-query";
-import { useAtomValue } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 
 import fetchData from "../utils/axios";
 
-import { userAtom, testDetailAtom, missionAtom } from "../atoms/atoms";
+import {
+  userAtom,
+  testDetailAtom,
+  missionAtom,
+  errorMessage,
+} from "../atoms/atoms";
 import Loading from "../components/shared/Loading";
 
 function usePostTest() {
   const testDetail = useAtomValue(testDetailAtom);
   const missions = useAtomValue(missionAtom);
   const { userId } = useAtomValue(userAtom);
+  const setErrorMessage = useSetAtom(errorMessage);
   const navigate = useNavigate();
 
   const payload = {
@@ -29,6 +35,11 @@ function usePostTest() {
     mutationFn: saveTest,
     onSuccess: () => {
       navigate("/dashboard");
+    },
+    onError: (error) => {
+      const message =
+        error?.response?.data?.error || "테스트 생성에 실패했습니다.";
+      setErrorMessage(message);
     },
   });
 
